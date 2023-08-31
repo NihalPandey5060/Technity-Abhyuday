457823d9d0f20b849d61495498e7692189edc860
Nihal Pandey <117839352+neurotoxin5060@users.noreply.github.com>
2023-08-31 10:10:45 +0530

    removed api key of omdb and telegram-bot

--- Task-6/task-06.py
+++ Task-6/task-06.py
@@ -2,9 +2,9 @@ from typing import Final
 import telebot
 import requests
 
-token: Final ='6333824276:AAEcDTYjzk2TMSGFKXqCnR48wECFWklsrgs'  # telegram-tb token
+token: Final =''  # telegram-tb token
 
-omdb_key='5da395eb1edb6485b675e67dfbb34b9d'                     # omdb token
+omdb_key=''                     # omdb token
 
 
 
@@ -14,7 +14,7 @@ def getMovie(message):
 
     tb.reply_to(message, 'Getting movie info...')
     movie_name = message.text.split(' ',1)[1]  
-    api_url = "https://www.omdbapi.com/?apikey=af752dc1&t="+movie_name
+    api_url = "https://www.omdbapi.com/?apikey=''&t="+movie_name
     response = requests.get(api_url)
     
     if response.status_code == 200:
